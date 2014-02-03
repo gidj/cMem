816ab69bf576c83c7e8c0ccd99bcb426a1cec4da
Gideon VanRiette <gvanriette@gmail.com>
2014-02-04 08:37:50 +0900

    Added comment about exceptins in mem.c

--- mem.c
+++ mem.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include "mem.h"
 
+/* Currently all of the allocatin functions take a pointer to a file and a line
+ * in that file. This is not used right now, but will be part of an expection
+ * interface that I will implement at a future time. */ 
+
+/* TODO: add implementation of exception interface*/ 
 
 extern void* mem_alloc(size_t nbytes, const char *file, int line)
 {
